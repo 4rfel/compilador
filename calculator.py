d41261a643fdf1320ef003c336c0b2f0c2374f6a
4rfel <rafaels6@al.insper.edu.br>
2021-02-22 21:48:13 -0300

    stopped using regex

--- calculator.py
+++ calculator.py
@@ -1,19 +1,28 @@
 import sys
-import re
 
-numbers = re.split("[+-]", sys.argv[1])
+def split_by_list(l, s):
+    s = s.split(l[0])
+    for i in l[1:]:
+        temp = []
+        for j in s:
+            temp += j.split(i)
+        s = temp
+    return s
+
+numbers = split_by_list(["+", "-"], sys.argv[1])
 numbers = [x.strip() for x in numbers]
 for i in numbers:
     for j in i:
         if j == " ":
             print("found a number with a space in the middle")
             exit(1)
-
 if numbers[0] == "":
     numbers[0] = 0
 numbers = [float(x) for x in numbers]
 
-operators = re.split("[0-9]", sys.argv[1].replace(" ", ""))
+l = [str(x) for x in range(10)]
+operators = sys.argv[1].replace(" ", "")
+operators = split_by_list(l, operators)
 operators = list(filter(lambda a: a != "", operators))
 
 result = numbers[0]
