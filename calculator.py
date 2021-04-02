68c7a28d24e5c2f376487c172ab976d205ce80a5
4rfel <38050598+4rfel@users.noreply.github.com>
2021-04-02 13:59:34 -0300

    using ast

--- calculator.py
+++ calculator.py
@@ -1,11 +1,57 @@
 import sys
-
+from abc import ABC, abstractmethod
 
 class Token:
 	def __init__(self, tipo: str, value: int):
 		self.tipo: str = tipo
 		self.value: int = value
 
+
+class Node(ABC):
+	def __init__(self, value = 0, children = []):
+		self.value = value
+		self.children = children
+
+	@abstractmethod
+	def Evaluate(self):
+		return
+
+class BinOP(Node):
+	def Evaluate(self):
+		if len(self.children) != 2:
+			sys.exit("BinOP diferente de 2 filhos")
+
+		if self.value == "add":
+			return self.children[0].Evaluate() + self.children[1].Evaluate()
+		if self.value == "sub":
+			return self.children[0].Evaluate() - self.children[1].Evaluate()
+		if self.value == "mult":
+			return self.children[0].Evaluate() * self.children[1].Evaluate()
+		if self.value == "div":
+			return self.children[0].Evaluate() // self.children[1].Evaluate()
+
+		sys.exit("DEU RUIM")
+
+class UnOp(Node):
+	def Evaluate(self):
+		if len(self.children) != 1:
+			sys.exit("UnOp diferente de 1 filho")
+		if self.value == "add":
+			return self.children[0].Evaluate()
+		if self.value == "sub":
+			return -self.children[0].Evaluate()
+		
+		sys.exit("DEU RUIM 2")
+
+class IntVal(Node):
+	def Evaluate(self):
+		if len(self.children) != 0:
+			sys.exit("IntVal tem Filho")
+
+		return self.value
+
+class NoOp(Node):
+	pass	
 
 accepted_chars = "[-+*/ 0-9()]"
 
@@ -39,8 +85,8 @@ class Tokenizer:
 			self.actual = Token(tipo="integer", value=int(temp))
 
 		elif self.origin[self.position : self.position + 2] == "/*":
-			self.origin = self.origin[:self.position+2] + " " + self.origin[self.position+2:]
 			self.actual = Token(tipo="comentary", value=0)
+			self.position += 2
 			while self.origin[self.position : self.position + 2] != "*/":
 				self.position += 1
 				if self.position >= len(self.origin):
@@ -49,11 +95,11 @@ class Tokenizer:
 
 
 		elif self.origin[self.position] == "+":
-			self.actual = Token(tipo="symbol", value=1)
+			self.actual = Token(tipo="add", value=1)
 			self.position += 1
 
 		elif self.origin[self.position] == "-":
-			self.actual = Token(tipo="symbol", value=-1)
+			self.actual = Token(tipo="sub", value=-1)
 			self.position += 1
 
 		elif self.origin[self.position] == "*":
@@ -95,14 +141,14 @@ class Parser:
 	def parseFactor(self):
 		self.getNextNotComentary()
 		if self.tokens.actual.tipo == "integer":
-			total = self.tokens.actual.value
+			value = self.tokens.actual.value
 			self.getNextNotComentary()
 			if self.tokens.actual.tipo == "integer":
 				sys.exit("2 integers seguidas")
-			return total
+			return IntVal(value)
 
 		if self.tokens.actual.tipo == "symbol":
-			return self.tokens.actual.value * self.parseFactor()
+			return UnOp(self.tokens.actual, self.parseFactor())
 
 		if self.tokens.actual.tipo == "open":
 			total = self.parseExpression()
@@ -115,30 +161,27 @@ class Parser:
 		sys.exit("2 mult/div seguidos")
 
 	def parseTerm(self):
-		total = self.parseFactor()
+		branch = self.parseFactor()
 		
 		while self.tokens.actual.tipo == "mult" or self.tokens.actual.tipo == "div":
-			if self.tokens.actual.tipo == "mult":
-				total *= self.parseFactor()
+			if self.tokens.actual.tipo == "mult" or self.tokens.actual.tipo == "div":
+				branch = BinOP(self.tokens.actual.tipo, [branch, self.parseFactor()])
 
-			elif self.tokens.actual.tipo == "div":
-				total //= self.parseFactor()
-
-		return total
+		return branch
 	
 			
 	def parseExpression(self):
-		total = self.parseTerm()
-		while self.tokens.actual.tipo == "symbol":			
-			if self.tokens.actual.tipo == "symbol":
-				signal = self.tokens.actual.value
-				total += signal * self.parseTerm()
+		branch = self.parseTerm()
+		while self.tokens.actual.tipo == "add" or self.tokens.actual.tipo == "sub":			
+			if self.tokens.actual.tipo == "add" or self.tokens.actual.tipo == "sub":
+				branch = branch = BinOP(self.tokens.actual.tipo, [branch, self.parseTerm()])
 
-		return int(total)
+		return branch
 
 	def run(self, code: str):
 		self.tokens = Tokenizer(code)
-		return self.parseExpression()
+		tree = self.parseExpression()
+		return tree.Evaluate()
 
 
 parser = Parser()
@@ -146,4 +189,7 @@ parser = Parser()
 if len(sys.argv) == 1:
 	sys.exit("sem input")
 
-print(parser.run(sys.argv[1]))
+with open(sys.argv[1], "r") as f:
+	code = f.read()
+# print(code)
+print(parser.run("1+1"))
